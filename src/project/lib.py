f524f5ee15aec1792b475f391fe3688ba2123c70
Nate Harada <nharada1@gmail.com>
2023-01-14 14:08:18 -0800

    first commit

---
+++ src/project/lib.py
@@ -0,0 +1,6 @@
+class ExampleClass():
+    def __init__(self, val: int = 1):
+        self.val = val
+    
+    def get_val(self) -> int:
+        return self.val
---
+++ src/project/main.py
@@ -0,0 +1,8 @@
+from project.lib import ExampleClass
+
+def main():
+    x = ExampleClass(5)
+    print(x.get_val())
+
+if __name__ == "__main__":
+    main()
